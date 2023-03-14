0eb504cd1a76304e880129dda69e5785c4e578d7
dylanjasonjones123 <127862093+dylanjasonjones123@users.noreply.github.com>
2023-03-14 13:48:41 +0000

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer>
+  <p class = "footer-text">
+    Contact us <br>
+    scaldygains@gmail.com <br>
+    IG @scaldygains <br>
+    TikTok @scaldygains <br>
+  </p>
+
+  <ul class="footer-img">
+    <li><img src="img/fb.png" class="fb-foot"></li>
+    <li><img src="img/tt.png" class="tt-foot"></li>
+    <li><img src="img/insta.png" class="insta-foot"></li>
+    <li><img src="img/yt.png" class="yt-foot"></li>
+  </ul>
+
+
+</footer>
