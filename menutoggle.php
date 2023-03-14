5fffa6543d1de2c4d6dff4909948bbc84de2dea2
dylanjasonjones123 <127862093+dylanjasonjones123@users.noreply.github.com>
2023-03-14 13:49:11 +0000

    Create menutoggle.php

---
+++ menutoggle.php
@@ -0,0 +1,16 @@
+<section>
+  <div class="menu-box">
+
+
+
+  <ul class = "toggle-menu">
+    <li><a href="#">About</a></li>
+    <li><a href="#">Gallery</a></li>
+    <li><a href="#">Contact</a></li>
+    <li><a href="#" class = "soon">Coming<br> Soon</a></li>
+
+
+</ul>
+</div>
+
+</section>
