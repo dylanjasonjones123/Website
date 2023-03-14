6ae20d18d01ae9df9e1ed7af7b8335b46f7f0e69
dylanjasonjones123 <127862093+dylanjasonjones123@users.noreply.github.com>
2023-03-14 13:43:18 +0000

    Create header.php

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, intitial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <a href=index.php><img src="img/scaldylogo.png" alt="Scaldy Logo" class="scaldylogo"></a>
+
+      <ul class = "top">
+        <li><a href="login.php" class="login">Login/Register</a></li>
+      </ul>
+
+      <ul class = "left">
+        <li><a href="about.php">About</a></li>
+        <li><a href="gallery.php">Gallery</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li><a href="comingsoon.php">Coming Soon</a></li>
+
+
+    </ul>
+
+    <ul class = "right">
+      <li><a href=facebook.co.uk><img src="img/fb.png" class="fb"></li></a>
+        <li><a href=><img src="img/tt.png" class="tt"></li></a>
+      <li><a href=#><img src="img/insta.png" class="insta"></li></a>
+      <li><a href=#><img src="img/yt.png" class="yt"></li></a>
+    </ul>
+
+    <ul class = "top2">
+      <li><a href="#" class="login">Login/Register</a></li>
+    </ul>
+
+    <ul>
+      <a href ="#"><li class="---" onclick="menuToggle()">
+        --<br>--<br>--
+      </li><a/>
+    </ul>
+
+
+  </header>
