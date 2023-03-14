1c7340dd1924945780cd3668d1b92c1ac23beaff
dylanjasonjones123 <127862093+dylanjasonjones123@users.noreply.github.com>
2023-03-14 13:49:36 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  include_once 'header.php';
+
+ ?>
+
+ <?php
+   include_once 'menutoggle.php';
+
+  ?>
+  <script src=script.js>
+
+  </script>
+
+
+  <?php
+    include_once 'footer.php';
+
+   ?>
+  </body>
+
+
+</html>
